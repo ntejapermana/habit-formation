7a571f26409bb5c39a034d3d17b17e606e39c564
Nugraha Tejapermana <nugraha.tejapermana@gdpventure.com>
2014-10-02 19:21:53 +0700

    Add webconfig and rootconfig on xml format

--- src/main/java/com/duskclouds/habit/config/WebConfig.java
+++ src/main/java/com/duskclouds/habit/config/WebConfig.java
@@ -90,9 +90,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	}
 	
 	 @Override
-	    public void addViewControllers(ViewControllerRegistry registry) {
-	        registry.addViewController("/login").setViewName("login");
-	        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-	    }
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
 
 }
